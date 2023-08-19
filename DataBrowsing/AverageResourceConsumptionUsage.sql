524a641d27985f24ec94a9db227e620cb6c19b34
Stanislaw Horna <stanislawhorna@outlook.com>
2023-08-19 14:00:38 +0200

    modified:   AverageResourceConsumptionUsage.sql

--- DataBrowsing/AverageResourceConsumptionUsage.sql
+++ DataBrowsing/AverageResourceConsumptionUsage.sql
@@ -1,5 +1,5 @@
 SELECT
-    DNSHostName AS 'Name',
+    SUBSTRING(DNSHostName,1,(CHARINDEX('.',DNSHostName) - 1)) AS 'Name',
     AVG(CPU_time_Percentage) AS 'CPU usage',
     AVG(RAM_usage_Percentage) AS 'RAM usage',
     AVG(Disk_time_Percentage) AS 'Disk usage',
