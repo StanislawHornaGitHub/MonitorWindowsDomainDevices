b82b371aa104e4b545d0e84e26513830f7f8dc8e
Stanislaw Horna (Server-Automation) <stanislawhorna@outlook.com>
2023-08-19 09:38:36 +0200

    modified:   Core/Import-AllModules.psm1

--- Core/Import-AllModules.psm1
+++ Core/Import-AllModules.psm1
@@ -1,3 +1,6 @@
+############################
+## Import Modules SECTION ##
+############################
 Import-Module .\Core\JenkinsDependencies\Jenkins.psm1
 Import-Module .\Core\Functions\OverallDataTransformation.psm1
 Import-Module .\Core\SQL\Module\SQL.psm1
@@ -12,8 +15,12 @@ New-Variable -Name "SQL_PRIMARY_KEY" -Value "DNSHostName" -Force -Scope Global -
 New-Variable -Name "SQL_WRITE_TABLE_DATA_TEMPLATE" -Value "$SQL_QUERIES_DIRECTORY/UpdateDataTableTemplate.sql" -Force -Scope Global -Option ReadOnly
 New-Variable -Name "SQL_ACTIVE_DEVICES_QUERY" -Value "$SQL_QUERIES_DIRECTORY/ActiveDevices.sql" -Force -Scope Global -Option ReadOnly
 
-
+###########################
+## Env Variables Section ##
+###########################
 $env:DEVICE_MONITORING_ROOT_DIRECTORY = $((Get-Location).Path)
+
+
 New-Variable `
     -Name "DEVICES_RUNNING_OTHER_OS_THAN_WIN" `
     -Value @("mac-mini.horna.local") `
