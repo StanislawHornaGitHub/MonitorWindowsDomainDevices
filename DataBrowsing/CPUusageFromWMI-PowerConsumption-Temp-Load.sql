776ad26f7ed7a4a9b92a8fbb06906e255f1001a3
Stanislaw Horna <stanislawhorna@outlook.com>
2023-08-22 23:42:54 +0200

    CPUusageFromWMI-PowerConsumption-Temp-Load.sql

--- DataBrowsing/CPUusageFromWMI-PowerConsumption-Temp-Load.sql
+++ DataBrowsing/CPUusageFromWMI-PowerConsumption-Temp-Load.sql
@@ -7,7 +7,7 @@ SELECT
     ROUND(AVG(Network_Total_Mbps),3,1) AS 'Network Traffic Mbps',
     ROUND(AVG(PowerAndTemperature.CPU_Temperature_Max),1,1) AS 'CPU Temperature C',
     ROUND(AVG(PowerAndTemperature.PowerConsumption_MAX),1,1) AS 'PowerConsumption W',
-    COUNT(WMIload.CPUusage) AS 'Number of measurements'
+    WMIload.Measurement AS 'Number of measurements'
 FROM Hardware
 LEFT JOIN PowerAndTemperature 
     On Hardware.DNSHostName = PowerAndTemperature.DNSHostName
@@ -15,9 +15,10 @@ JOIN PerformanceCounters
     On Hardware.DNSHostName = PerformanceCounters.DNSHostName
 LEFT JOIN (SELECT 
     DNSHostname,
-    ROUND(AVG(CPU_Load_Percentage),1,1)AS 'CPUusage'
+    ROUND(AVG(CPU_Load_Percentage),1,1) AS 'CPUusage',
+    COUNT(CPU_Load_Percentage) AS 'Measurement'
 FROM ResourceConsumption_OLD
 Where ResourceConsumption_OLD.TimeStamp > '2023-08-20'
 GROUP BY DNSHostName) WMIload ON WMIload.DNSHostName = Hardware.DNSHostName
-GROUP BY Hardware.DNSHostName, DeviceModel, WMIload.CPUusage
+GROUP BY Hardware.DNSHostName, DeviceModel, WMIload.CPUusage, WMIload.Measurement
 ORDER BY 'PowerConsumption W' DESC
