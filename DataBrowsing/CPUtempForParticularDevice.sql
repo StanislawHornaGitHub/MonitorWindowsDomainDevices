e84f31446a54ec272dbd3b49860556abbfc71ee0
Stanislaw Horna <stanislawhorna@outlook.com>
2023-08-25 11:14:37 +0200

    modified:   CPUtempForParticularDevice.sql

--- DataBrowsing/CPUtempForParticularDevice.sql
+++ DataBrowsing/CPUtempForParticularDevice.sql
@@ -1,9 +1,10 @@
-DECLARE @DeviceDNSHostname AS NVARCHAR(50) = 'Lian6700.Horna.local'
-
+DECLARE @DeviceDNSHostname AS NVARCHAR(50) = 'Lian6700'
+SELECT CONCAT(@DeviceDNSHostname, '.Horna.local') AS 'Device Name'
 SELECT TOP(10)
     [TimeStamp] AS 'Time',
     ROUND(CPU_Temperature_Current,1,1) AS 'CPU Temperature C',
+    ROUND(GPU_Temperature_Current,1,1) AS 'GPU Temperature C',
     ROUND(PowerConsumption_Current,1,1) AS 'Power Consumption W'
 FROM PowerAndTemperature
-WHERE DNSHostName = @DeviceDNSHostname
+WHERE DNSHostName = CONCAT(@DeviceDNSHostname, '.Horna.local')
 ORDER BY [TimeStamp] DESC
