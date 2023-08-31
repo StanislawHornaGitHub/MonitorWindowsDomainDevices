a2c2a54d53a5ab960e15f0c584b03b7194b7ef37
Stanislaw Horna (Server-Automation) <stanislawhorna@outlook.com>
2023-09-01 00:33:18 +0200

    modified:   Deploy-OpenHardwareMonitor.ps1

--- Core/Deployments/Deploy-OpenHardwareMonitor.ps1
+++ Core/Deployments/Deploy-OpenHardwareMonitor.ps1
@@ -1,11 +1,36 @@
 <#
-    .DESCRIPTION
+.SYNOPSIS
     Script to Deploy Open Hardware Monitor
+
+.DESCRIPTION
+    Script responsible for deploying Open Hardware Monitor components,
+    which are required to capture CPU and GPU temperature, as well as Power Consumption.
+    Script is connecting to each device which where such information can be monitor.
+    Following steps are performed:
+    1. Creating local folder on each device to store additional components
+    2. Downloading components from Network share
+    3. Update Inventory table for devices where deployment was successfull
+
+.INPUTS
+    None
+
+.OUTPUTS
+    None
+
+.NOTES
+
+    Version:            1.0
+    Author:             Stanisław Horna
+    Mail:               stanislawhorna@outlook.com
+    GitHub Repository:  https://github.com/StanislawHornaGitHub/MonitorWindowsDomainDevices
+    Creation Date:      22-Aug-2023
+    ChangeLog:
+
+    Date            Who                     What
 
 #>
 Import-Module "./Core/Import-AllModules.psm1"
 New-Variable -Name "EXIT_CODE" -Value 0 -Force -Scope Script
-New-Variable -Name "CREDENTIAL" -Value $(Get-CredentialFromJenkins) -Force -Scope Script -Option ReadOnly
 
 New-Variable -Name 'DEPLOYMENT_SERVER_FOLDER_NAME_TO_COPY' -Value "OpenHardwareMonitor" -Force -Scope Script
 New-Variable -Name "SQL_DEVICES_TO_DEPLOYMENT" -Value "$SQL_QUERIES_DIRECTORY\DevicesToDeployOpenHardwareMonitor.sql"
@@ -31,27 +56,30 @@ function Invoke-Deployment {
     foreach ($C in $Computers) {
         Write-host "$($C.DNSHostName)"
         try {
-            Invoke-Command -ComputerName $($C.DNSHostName) -Credential $CREDENTIAL -ScriptBlock {
+            Invoke-Command -ComputerName $($C.DNSHostName)-ScriptBlock {
                 param(
                     $LOCAL_MONITORING_COMPONENTS_PATH,
                     $DEPLOYMENT_SERVER_FOLDER_NAME_TO_COPY,
-                    $DEPLOYMENT_SERVER_SHARE,
-                    [PSCredential] $CREDENTIAL
+                    $DEPLOYMENT_SERVER_SHARE
                 )
+                # Create local directory for additional components if such does not exist
                 if (-not (Test-Path -Path $LOCAL_MONITORING_COMPONENTS_PATH)) {
                     New-Item -ItemType Directory -Path $LOCAL_MONITORING_COMPONENTS_PATH | Out-Null
                 }
-                New-PSDrive -Name "Deployment" -Root $DEPLOYMENT_SERVER_SHARE -PSProvider FileSystem -Credential $CREDENTIAL -ErrorAction Stop | Out-Null
+                # Map network share to download the components folder
+                New-PSDrive -Name "Deployment" -Root $DEPLOYMENT_SERVER_SHARE -PSProvider FileSystem -ErrorAction Stop | Out-Null
+                # Download component folder
                 Copy-Item -Path "Deployment:\$DEPLOYMENT_SERVER_FOLDER_NAME_TO_COPY" `
                     -Destination $LOCAL_MONITORING_COMPONENTS_PATH `
                     -Recurse `
                     -Force `
                     -Confirm:$false `
                     -ErrorAction Stop
-            } -ArgumentList $LOCAL_MONITORING_COMPONENTS_PATH, $DEPLOYMENT_SERVER_FOLDER_NAME_TO_COPY, $DEPLOYMENT_SERVER_SHARE, $CREDENTIAL `
+            } -ArgumentList $LOCAL_MONITORING_COMPONENTS_PATH, $DEPLOYMENT_SERVER_FOLDER_NAME_TO_COPY, $DEPLOYMENT_SERVER_SHARE `
                 -ErrorAction Stop
+            # Update Inventory table
             $QueryToInvoke = $UpdateIsOHMdeployed.Replace("COMPUTER_DNS_HOSTNAME_VARIABLE", $($C.DNSHostName))
-            Invoke-SQLquery -Query $QueryToInvoke -Credential $CREDENTIAL
+            Invoke-SQLquery -Query $QueryToInvoke
         }
         catch {
             Write-host "$($C.DNSHostName) - Deployment was not successfull"
