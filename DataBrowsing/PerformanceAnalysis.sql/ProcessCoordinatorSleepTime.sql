1a27f5710b6dfc7a6689ac1bf1f46c408710f305
Stanislaw Horna <stanislawhorna@outlook.com>
2023-09-13 22:32:43 +0200

    StartTime and EndTime added

--- DataBrowsing/PerformanceAnalysis.sql/ProcessCoordinatorSleepTime.sql
+++ DataBrowsing/PerformanceAnalysis.sql/ProcessCoordinatorSleepTime.sql
@@ -1,5 +1,8 @@
+DECLARE @StartDate AS Date = '2023-09-12'
+DECLARE @EndDate AS Date = '2023-09-13'
+
 SELECT 
     AVG(CAST(SUBSTRING([Message], PATINDEX('%[0-9]%', [Message]), PATINDEX('%[0-9][^0-9]%', [Message] + 't') - PATINDEX('%[0-9]%', 
                     [Message]) + 1) as int)) AS 'Average Process Coordinator Sleep time [ms]'
 FROM Log_Process_coordinator
-WHERE Message_type = 'SLEEP'
+WHERE Message_type = 'SLEEP' AND [Timestamp] > @StartDate AND [Timestamp] < @EndDate
