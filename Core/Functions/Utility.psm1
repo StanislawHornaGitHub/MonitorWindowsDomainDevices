e57eb15b5c2d5432d14527f4c5f0178dee69e117
Stanislaw Horna (Server-Automation) <stanislawhorna@outlook.com>
2023-09-30 09:42:14 +0200

    modified:   Core/Functions/Utility.psm1

--- Core/Functions/Utility.psm1
+++ Core/Functions/Utility.psm1
@@ -1,3 +1,28 @@
+<#
+.SYNOPSIS
+    Module with functions to retrieve information from monitored devices.
+
+.DESCRIPTION
+    Module consists of functions used get necessary data required to start collecting info from devices
+    and those which are starting separate background job for each device to collect data.
+
+    It contain following functions:
+        Get-CompletedJobName - used to get the job name of completed job
+        Remove-RemainingJobs - remove jobs which did not ended
+        Get-DevicesList - Get devices list to process
+
+.NOTES
+
+    Version:            1.0
+    Author:             Stanisław Horna
+    Mail:               stanislawhorna@outlook.com
+    GitHub Repository:  https://github.com/StanislawHornaGitHub/MonitorWindowsDomainDevices
+    Creation Date:      28-Aug-2023
+    ChangeLog:
+
+    Date            Who                     What
+
+#>
 function Get-CompletedJobName {
     $jobName = $null
     $jobName = (Get-Job | Where-Object { $_.State -ne "Running" } | Select-Object -First 1).Name
@@ -9,4 +34,25 @@ function Remove-RemainingJobs {
         $remainingJobs | Remove-Job -Force
         Write-Joblog -Message "Background jobs were running longer than REMOTE_CONNECTION_TIMEOUT_SECONDS ($REMOTE_CONNECTION_TIMEOUT_SECONDS)"
     }
+}
+function Get-DevicesList {
+    param (
+        [bool]$RunOutOfSchedule,
+        $QueryWithSchedule,
+        $QueryOutOfSchedule
+    )
+    if($RunOutOfSchedule -eq $true){
+        $Computer = Get-ComputerListToProcess -PredefinedQuery $QueryOutOfSchedule
+    }else{
+        $Computer = Get-ComputerListToProcess -PredefinedQuery $QueryWithSchedule
+    }
+    if($null -eq $Computer){
+        return $null
+    }
+    if($null -eq $Computer.count){
+        New-Variable -Name "PROCESSED_DEVICES" -Value 1 -Force -Scope Global -Option ReadOnly
+    }else{
+        New-Variable -Name "PROCESSED_DEVICES" -Value $($Computer.count) -Force -Scope Global -Option ReadOnly
+    }
+    return $Computer
 }
